02d0e46f867fcbc6801ed9a358c83e9cef989faf
korkija <49477387+korkija@users.noreply.github.com>
2024-08-28 17:38:55 +0200

    Initialize Branch-3-test

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {Router} from "react-router-dom";
+import {Layout} from 'antd';
+import Main from '../src/routs';
+import {createBrowserHistory} from "history";
+import {Provider} from "react-redux";
+import {store} from "./store";
+
+export const history = createBrowserHistory();
+
+const {Content, Footer} = Layout;
+
+function App() {
+    return (
+        <Provider store={store}>
+            <Layout>
+                <Router history={history}>
+                    <Content style={{padding: '0 50px', marginTop: 64}}>
+                        <div style={{background: '#eee', padding: 24, minHeight: 380}}>
+                            <Main/>
+                        </div>
+                    </Content>
+                </Router>
+                <Footer>Hello world ©2021© Created by hand</Footer>
+                <Footer>Hello world ©2021© Created by hand</Footer>
+                <Footer>world ©2021© Created by hand</Footer>
+                <Footer>Hello ©2021© Created by hand</Footer>
+                <Footer>Hello ©2021© by hand</Footer>
+            </Layout>
+        </Provider>
+    );
+}
+
+export default App;
